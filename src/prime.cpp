3df3ebc0bf68b67eea861b2788bb599b33bb2969
Mikołaj Franek <mail.mikolaj.franek@gmail.com>
2026-02-01 21:50:54 +0100

    hello world primes in eclipse

---
+++ src/prime.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : prime.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World primes!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
